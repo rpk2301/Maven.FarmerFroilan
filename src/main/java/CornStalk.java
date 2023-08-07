01065069691fd095c63f006fada0cc9a86c4f433
NigelBynum <136828778+NigelBynum@users.noreply.github.com>
2023-08-07 19:28:51 -0400

    update 3

---
+++ src/main/java/CornStalk.java
@@ -0,0 +1,9 @@
+public class CornStalk extends EdibleEarCorn {
+    CornStalk yield(boolean hasBeenFertilized, boolean hasBeenHarvested) {
+        if (hasBeenHarvested == true && hasBeenHarvested == true) {
+
+            return this;
+        }
+        return null;
+    }
+}
---
+++ src/main/java/TomatoPlant.java
@@ -0,0 +1,9 @@
+public class TomatoPlant extends EdibleTomato{
+    TomatoPlant yield(boolean hasBeenFertilized, boolean hasBeenHarvested) {
+        if (hasBeenHarvested == true && hasBeenHarvested == true) {
+
+            return this;
+        }
+        return null;
+    }
+}
