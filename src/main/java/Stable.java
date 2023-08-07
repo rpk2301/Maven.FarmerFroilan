27e68b0f460141498275162721a9f4f351aefa4e
DestanyM <destanymcclellan22@gmail.com>
2023-08-07 16:50:08 -0400

    Field completed

--- src/main/java/Stable.java
+++ src/main/java/Stable.java
@@ -2,8 +2,6 @@ import java.util.ArrayList;
 
 public class Stable {
 
-
-
     ArrayList<Horse> horses = new ArrayList<Horse>();
 
     public ArrayList<Horse> getHorses() {
@@ -12,6 +10,14 @@ public class Stable {
 
     public void setHorses(ArrayList<Horse> horses) {
         this.horses = horses;
+    }
+
+    public Horse getPerson(int index) {
+        if (index >= 0 && index < horses.size()) {
+            return horses.get(index);
+        }
+        return null;
+
     }
 
 
